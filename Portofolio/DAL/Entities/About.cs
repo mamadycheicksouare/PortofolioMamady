418138751e11d672ab4a33767e0cc4d79007d072
mamady <mamadycheicksouare@gmail.com>
2025-08-13 21:08:43 +0300

    Updated

---
+++ Portofolio/DAL/Entities/About.cs
@@ -0,0 +1,13 @@
+namespace Portofolio.DAL.Entities;
+
+public class About
+{
+    public int Id { get; set; }
+    
+    public string Title { get; set; }
+    
+    public string Description { get; set; }
+
+    public string Details { get; set; }
+    
+}
---
+++ Portofolio/DAL/Entities/Experience.cs
@@ -0,0 +1,16 @@
+namespace Portofolio.DAL.Entities;
+
+public class Experience
+{
+    public int Id { get; set; }
+
+    public string Company  { get; set; }
+
+    public string Position { get; set; }
+
+    public string Date { get; set; }
+    
+    public string Description { get; set; }
+    
+    
+}
